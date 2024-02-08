6f03e0b465dfaa2a7de3d6ac67560ac8bbda6c17
Dalton Riehl <dtonriehl@gmail.com>
2024-02-08 16:12:15 -0600

    Array Lab Complete: Achievement Get!

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
 // Write your solution here!
+const cats = ["Milo", "Otis", "Garfield"];
+function destructivelyAppendCat(name) {
+    cats.push("Ralph");
+}
+function destructivelyPrependCat(name) {
+    cats.unshift("Bob");
+}
+function destructivelyRemoveLastCat(name) {
+    cats.pop();
+}
+function destructivelyRemoveFirstCat(name) {
+    cats.shift();
+}
+function appendCat(name) {
+   const copyOfCats = [...cats.slice(), name];
+   return copyOfCats;
+}
+function prependCat(name) {
+    const copyOfCats = [name, ...cats.slice()];
+    return copyOfCats;
+}
+function removeFirstCat(name) {
+    const copyOfCats = cats.slice(1);
+    return copyOfCats;
+}
+function removeLastCat(name) {
+    const copyOfCats = cats.slice(0,2);
+    return copyOfCats;
+}
